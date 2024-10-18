01df9e66590a67bdf8aa73e015d4199da00d5fce
Steve Chan <steve@deepgenomics.com>
2024-10-18 14:38:59 -0400

    rewind dims, then rewind stride

--- src/genome_track.h
+++ src/genome_track.h
@@ -68,14 +68,13 @@ namespace detail {
 		template <typename T>
 		INLINE static void apply(T* RESTRICT& dst, size_t i, int& k, int dim, int stride, T value)
 		{
-			*dst = value;
-			dst += stride;
+			*(dst++) = value;
 
 			// If we've just written the last dimension of this position,
 			// then advance to the first dimension of the next  position
 			// to be written to, which is backwards, not forwards.
 			if (--k == 0) {
-				dst -= 2*stride;
+				dst -= dim + stride;
 				k = dim;
 			}
 		}

27edc4e71e9528cf0723841734bcfff10e6ebaaa
Avishai Weissberg <avishai@deepgenomics.com>
2025-01-10 07:33:09 -0800

    feat: support refseq anno GCF_000001405.40-RS_2024_08
    fixes #124

---
+++ data-src/hg38.p14/NCBI/hg38.p14_RS_2024_08/fetch.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+source ../../../functions.sh
+wget_ncbi_hg38 hg38.p14_RS_2024_08.gff3.gz https://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/annotation_releases/GCF_000001405.40-RS_2024_08/GCF_000001405.40_GRCh38.p14_genomic.gff.gz

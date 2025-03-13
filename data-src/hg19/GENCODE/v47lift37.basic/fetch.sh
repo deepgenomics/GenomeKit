0aa331f2ca0eafb3081c114bd79f1edf47b911bf
Avishai Weissberg <avishai@deepgenomics.com>
2025-03-13 11:20:04 -0700

    feat: add gencode.v47 adjacent annos + appris
    fixes #134

---
+++ data-src/hg19/GENCODE/v47lift37.basic/fetch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -O v47lift37.basic.gff3.gz ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_47/GRCh37_mapping/gencode.v47lift37.basic.annotation.gff3.gz
---
+++ data-src/hg38.p14/GENCODE/v47.basic/fetch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -O v47.basic.gff3.gz ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_47/gencode.v47.basic.annotation.gff3.gz

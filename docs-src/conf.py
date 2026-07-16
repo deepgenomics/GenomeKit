93977cfb783e332c40d9e421f1309dc8d5046c8a
Declan Lim <declan.lim@deepgenomics.com>
2026-07-16 11:53:33 -0400

    update python docs generation version

--- docs-src/conf.py
+++ docs-src/conf.py
@@ -297,7 +297,7 @@ texinfo_documents = [
 #texinfo_no_detailmenu = False
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('https://docs.python.org/3.7/', None),
+intersphinx_mapping = {'python': ('https://docs.python.org/3.12/', None),
                        'numpy': ('http://docs.scipy.org/doc/numpy/', None)}
 
 # Order members according to their source order, rather than alphabetically

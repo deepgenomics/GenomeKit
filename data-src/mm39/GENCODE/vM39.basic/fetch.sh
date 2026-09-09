c41117a24c367a8bca67770104558d26445a2263
Jeff Wintersinger <jwintersinger@users.noreply.github.com>
2026-09-09 13:38:16 -0400

    feat: add mouse GENCODE M39 annotations (#252)
    Defer APPRIS until matching M39 / Ensembl 116 data is available.

---
+++ data-src/mm39/GENCODE/vM39.basic/fetch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -O vM39.basic.gff3.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M39/gencode.vM39.basic.annotation.gff3.gz
---
+++ data-src/mm39/GENCODE/vM39/fetch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -O vM39.gff3.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M39/gencode.vM39.annotation.gff3.gz

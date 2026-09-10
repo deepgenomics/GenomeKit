cdcafa89f6731f5eec6b67416bc4cdd8262cfce9
Jeff Wintersinger <jwintersinger@users.noreply.github.com>
2026-09-10 16:29:45 -0400

    feat: add human GENCODE v50 annotations (#253)
    Defer APPRIS until matching v50 / Ensembl 116 data is available.

---
+++ data-src/hg38.p14/GENCODE/v50.basic/fetch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -O v50.basic.gff3.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_50/gencode.v50.basic.annotation.gff3.gz
---
+++ data-src/hg38.p14/GENCODE/v50/fetch.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+wget -O v50.gff3.gz https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_50/gencode.v50.annotation.gff3.gz
